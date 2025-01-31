06b3743d9ee9b12cff1f58e1ab4fb3f9b7260ff2
Maxime Le Pelvé <maxime.le-pelve@insa-rennes.fr>
2025-01-31 17:14:22 +0100

    TP2 - prise en main de l'environement et débuts sur Android

--- app/src/main/java/com/insa/mygamelist/MainActivity.kt
+++ app/src/main/java/com/insa/mygamelist/MainActivity.kt
@@ -4,16 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.insa.mygamelist.data.Game
 import com.insa.mygamelist.data.IGDB
+import com.insa.mygamelist.data.getCoverUrl
+import com.insa.mygamelist.data.getGenre
 import com.insa.mygamelist.ui.theme.MyGamesListTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,11 +56,58 @@ class MainActivity : ComponentActivity() {
                         containerColor = Color.Magenta,
                         titleContentColor = Color.Black,
                     ), title = { Text("My Games List") })
-                }, modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    Text("À remplir", modifier = Modifier.padding(innerPadding))
-
+                }, modifier = Modifier.fillMaxWidth()) { innerPadding ->
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .background(Color.LightGray)
+                    ){
+                        items(IGDB.games.size){i ->
+                            GameItem(IGDB.games[i])
+                        }
+                    }
                 }
+            }
+        }
+    }
+}
+@Composable
+fun GameItem(game: Game){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+            .padding(16.dp)
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            AsyncImage(     // affichage de la couverture
+                model = "https:"+getCoverUrl(game),
+                contentDescription = "Image de"+game.name,
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
+
+            Spacer(modifier = Modifier.width(16.dp)) // espace entre photo et texte
+
+            Column{
+                Text(
+                    text =game.name, // affiche le nom du jeu
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold, // Gras
+                    textDecoration = TextDecoration.Underline, // Souligné
+                    color = Color.Black
+                )
+                Text(
+                    text = "Genres : "+ getGenre(game), // affiche la liste des genres
+                    fontSize = 18.sp,
+                    color = Color.Black
+                )
             }
         }
     }
--- app/src/main/java/com/insa/mygamelist/data/IGDB.kt
+++ app/src/main/java/com/insa/mygamelist/data/IGDB.kt
@@ -5,9 +5,18 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.insa.mygamelist.R
 
+/*
+Permet de récupérer tous les données présents dans les différents
+fichiers Json
+
+ */
 object IGDB {
 
     lateinit var covers: List<Cover>
+    lateinit var games: List<Game>
+    lateinit var genres: List<Genre>
+    lateinit var platform_logos: List<PlatformLogo>
+    lateinit var platforms: List<Platform>
 
     fun load(context: Context) {
         val coversFromJson: List<Cover> = Gson().fromJson(
@@ -15,8 +24,65 @@ object IGDB {
             object : TypeToken<List<Cover>>() {}.type
         )
 
+        val gamesFromJson: List<Game> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.games).bufferedReader(),
+            object : TypeToken<List<Game>>() {}.type
+        )
+
+        val genresFromJson: List<Genre> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.genres).bufferedReader(),
+            object : TypeToken<List<Genre>>() {}.type
+        )
+
+        val platformLogosFromJson: List<PlatformLogo> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platform_logos).bufferedReader(),
+            object : TypeToken<List<PlatformLogo>>() {}.type
+        )
+
+        val platformsFromJson: List<Platform> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platforms).bufferedReader(),
+            object : TypeToken<List<Platform>>() {}.type
+        )
+
+
         covers = coversFromJson
+        games = gamesFromJson
+        genres = genresFromJson
+        platform_logos = platformLogosFromJson
+        platforms = platformsFromJson
     }
 }
 
 data class Cover(val id: Long, val url: String)
+data class Game(val id: Long, val cover: Long, val first_release_date : Long, val genres: List<Int>, val name: String, val platforms: List<Int>, val summary: String, val total_rating: Double)
+data class Genre ( val id: Int, val name: String)
+data class PlatformLogo (val id : Int, val url: String )
+data class Platform (val id: Int, val name:String, val platform_logo: Int)
+
+
+/*Fonction qui permet de récupérer l'url à partir du numéro
+* présent dans game
+ */
+
+fun getCoverUrl(game : Game):String ?{
+    val cover = IGDB.covers.find{ it.id==game.cover}
+    return cover?.url
+}
+
+/* Fonction qui permet de récupérer la liste des genres
+* pour un jeux donné.
+* Chaque genre est séparé par une virgule
+*/
+fun getGenre (game : Game):String{
+    var res: String =""
+    for(i in game.genres){
+        val g = IGDB.genres.find{it.id==i}
+        if(g!=null){    // permet d'ajouter avant le genre si la liste n'est pas vide
+            if(res.isNotEmpty()){
+                res+=", "
+            }
+        }
+        res+=g?.name
+    }
+    return res
+}
